fa05efbba0dbc2174fb348a7535174d00c868fd1
Sameer2005-dot <jena80225@gmail.com>
2026-02-03 00:17:22 +0530

    Added Var and DataType

---
+++ JS/lect40 variables/index.js
@@ -0,0 +1,24 @@
+// Var //
+// This is globally and function scoped
+
+// var age = 20;
+
+// if(true){
+//     console.log(age);
+// }
+
+
+// function solve() {
+//     var age = 25;
+//     console.log(age);
+// }
+// solve();
+
+
+// Let //
+// This is Blocked Scoped
+
+{
+    let age = 23;
+    console.log(age);
+}
