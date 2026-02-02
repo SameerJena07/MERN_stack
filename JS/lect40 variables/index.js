584c8158ac8356e084ea579646ae5026c312a7ca
Sameer2005-dot <jena80225@gmail.com>
2026-02-03 00:46:59 +0530

    Added var and let

--- JS/lect40 variables/index.js
+++ JS/lect40 variables/index.js
@@ -7,18 +7,37 @@
 //     console.log(age);
 // }
 
-
 // function solve() {
 //     var age = 25;
 //     console.log(age);
 // }
 // solve();
 
+//Here Var we can declare multiple time
+// var x = 20;
+// var x = 25;
+
 
 // Let //
 // This is Blocked Scoped
 
-{
-    let age = 23;
-    console.log(age);
-}
+// {
+//     let age = 23;
+//     console.log(age);
+// }
+
+// here we can't declare multiple time only declare once
+// let a = 25;
+// let a = 30;
+
+// function example() {
+//     let a = 25;
+//     a = "Kuna";
+//     console.log(a);
+// }
+// example();
+
+
+
+
+
