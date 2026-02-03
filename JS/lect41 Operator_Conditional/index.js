c2350f1a4c1c5aaf53e780660e23e1697d1fa381
Sameer2005-dot <jena80225@gmail.com>
2026-02-04 00:18:33 +0530

    added Operation of js

---
+++ JS/lect41 Operator_Conditional/index.js
@@ -0,0 +1,33 @@
+
+// let a = 8
+// console.log(a++);
+// console.log(++a);
+
+
+
+// let a = 10
+// let b = 20;
+// console.log(a + b);
+
+
+let a = 15;
+
+// a = a + 5;
+a += 5
+console.log(a);
+
+// a = a - 5;
+a -= 5
+console.log(a);
+
+// a = a * 5;
+a *= 5
+console.log(a);
+
+// a = a / 5;
+a /= 5
+console.log(a);
+
+// a = a ** 5;
+a **= 5
+console.log(a);
