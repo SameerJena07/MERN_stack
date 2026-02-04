a6e9e548419881b51d478286bac85d46a464fdd8
Sameer2005-dot <jena80225@gmail.com>
2026-02-05 00:29:58 +0530

    Added ternary operator

--- JS/lect41 Operator_Conditional/index.js
+++ JS/lect41 Operator_Conditional/index.js
@@ -9,7 +9,7 @@
 // let b = 20;
 // console.log(a + b);
 
-
+/*
 let a = 15;
 
 // a = a + 5;
@@ -31,3 +31,16 @@ console.log(a);
 // a = a ** 5;
 a **= 5
 console.log(a);
+*/
+
+
+// let c = 5
+// let d = '5'
+// console.log(c == d);   //looses equality
+// console.log(c === d);  //Stict equality
+
+
+let age = 10;
+let vote = (age > 18) ? 'I can vote' : 'I cannot vote';  //Ternary Operator
+console.log(vote);
+
