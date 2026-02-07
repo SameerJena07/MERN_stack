02b0c35e3159a62e72bed9d428175a74f8e72139
Sameer2005-dot <jena80225@gmail.com>
2026-02-07 23:23:09 +0530

    Added strings

--- JS/lect42 Loops_String/index.js
+++ JS/lect42 Loops_String/index.js
@@ -57,10 +57,80 @@
 // while(i <= 10);
 
 
-let i = 5;
-do {
-    console.log(i);
+// let i = 5;
+// do {
+//     console.log(i);
 
-    i--;
+//     i--;
     
-} while (i > 0);
+// } while (i > 0);
+
+
+
+
+/// *** STRING *** ///
+
+
+
+// let firstName = "Sameer";
+// let lastName = 'Jena';
+
+// let Name = `sameer
+// jena  this is 
+// my name`;
+
+// console.log(Name);
+
+
+// let firstName = new String("Sameer Jena");
+// console.log(firstName);
+
+// console.log(typeof(firstName));
+
+
+
+// let opt1 = "English";
+// let opt2 = "Hindi";
+
+// let finalAns = `${opt1} ${opt2}`;
+// console.log(finalAns);
+
+
+// let ans = opt1 + " " + opt2;
+// console.log(ans);
+
+
+
+// let a = "english";
+// let b = "HINDI";
+
+// console.log(a.length);
+
+// console.log(a.toUpperCase());
+
+// console.log(b.toLowerCase());
+
+
+
+// SUB STRING //
+
+// let str = "Sameer";
+// console.log(str.substring(2));
+// console.log(str.substring(2, 4));
+
+
+// Split-string //
+
+// let sentence = "I am a student";
+// let word = sentence.split(" ");
+// console.log(word);
+// console.log(typeof(word));
+
+
+let newOne = "This\\is\\a\\small\\programme";
+let newWord = newOne.split("\\");
+console.log(newWord);
+
+console.log(newWord.join('-'));
+
+
