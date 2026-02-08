17bcc144f27bf9edabe15adcdd2df55305bedba2
Sameer2005-dot <jena80225@gmail.com>
2026-02-08 21:49:35 +0530

    Added Array

---
+++ JS/lect43 Array/index.js
@@ -0,0 +1,57 @@
+
+// * OBJECT * //
+// let user = {
+//     Name: "Sameer Jena",
+//     Address: "Khordha",
+//     age: 20,
+//     height: "5Feet 3Inch"
+// };
+
+// console.log(user);
+
+
+// let obj = {
+//     Name: "Sameer Jena",
+//     Address: "Khordha",
+//     age: 20,
+//     height: "5Feet 3Inch",
+//     greet: function(){
+//         console.log("I am a student");
+//     }
+// };
+
+// console.log(user);
+// user.greet();
+
+// let obj2 = obj;
+// console.log(obj);
+
+
+
+// * ARRAY * //
+
+// let arr = [1,2,3,4,5,6]
+// console.log(arr);
+
+
+// using constructor //
+
+let brr = new Array("sameer", 1, true);
+
+brr.push('sameer');
+brr.pop();
+
+brr.shift();
+brr.unshift("sameer jena");
+brr.push(20);
+brr.push(40);
+brr.push(70);
+
+// console.log(brr.slice(3,5));
+console.log(brr);
+
+
+
+// console.log(brr[1]);
+
+
