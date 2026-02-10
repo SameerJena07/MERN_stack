b78e97fc6a75b909eadee759380accf84e42e58e
Sameer2005-dot <jena80225@gmail.com>
2026-02-11 01:34:33 +0530

    Added call Stack in function

--- JS/lect45 callStack and hoisting/index.js
+++ JS/lect45 callStack and hoisting/index.js
@@ -33,3 +33,84 @@
 
 // }
 
+
+// * Call Stack * //
+
+
+// function greetMe (greet, fullName){
+//     console.log("Hello", fullName);
+//     greet();
+// }
+
+// function greet(){
+//     console.log("greet for the day");
+// }
+
+// greetMe(greet, "Sameer");
+
+
+/***/
+
+
+// function solve(number){
+//     return function(number){
+//         return (number*number);
+//     }
+// }
+
+// let ans = solve(5);
+// let finalAns = ans(10);
+// console.log(finalAns);
+
+
+/***/
+
+
+// const arr = [
+//     function(a,b){
+//         return a+b;
+//     },
+
+//     function(a,b){
+//         return a-b;
+//     },
+
+//     function(a,b){
+//         return a*b;
+//     }
+// ]
+
+// let first = arr[2];
+// let ans = first (10, 20);
+// console.log(ans);
+
+
+/***/
+
+
+// let obj = {
+//     age: 20,
+//     weight: 80,
+//     height: 5,
+//     greet: () => {
+//         console.log("Namaste, Duniya");
+        
+//     }
+// }
+
+// console.log(obj.weight);
+// obj.greet()
+
+
+/***/
+
+
+
+console.log(greet);
+
+var greet = function (){
+    console.log("Namaste Dunia");
+    
+}
+
+
