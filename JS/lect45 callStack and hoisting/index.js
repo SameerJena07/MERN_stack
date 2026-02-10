3bffc73f93d56f49e161ae476d972f6599389f6f
Sameer2005-dot <jena80225@gmail.com>
2026-02-11 00:52:11 +0530

    Added Hoisting

---
+++ JS/lect45 callStack and hoisting/index.js
@@ -0,0 +1,35 @@
+
+
+// console.log(age);
+// var age = 25;
+
+
+// console.log(age);
+// let age = 20;
+
+
+// console.log(age);
+// const age = 30;
+
+
+
+// sayName("Sameer");
+// function sayName(fullName) {
+//     console.log(fullName);    
+// }
+
+
+
+// sayHello();
+// let sayHello = function() {
+//     console.log("Hello, How are You?");
+    
+// }
+
+
+
+// const obj1 = new Human();
+// class Human {
+
+// }
+
