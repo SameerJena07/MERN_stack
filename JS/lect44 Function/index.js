1cd722007f3b566bd14d3d1c4e814ea574d8c2b8
Sameer2005-dot <jena80225@gmail.com>
2026-02-10 23:29:42 +0530

    update functions

--- JS/lect44 Function/index.js
+++ JS/lect44 Function/index.js
@@ -1,6 +1,33 @@
+// * Function * //
 
-function sayMyName() {
-    console.log("Sameer Jena");
+
+// function sayMyName() {
+//     console.log("Sameer Jena");
+// }
+
+// sayMyName();
+
+
+
+// function printCounting() {
+//     for (let i = 1; i<=100; i++){
+//         console.log(i);
+        
+//     }
+// }
+// printCounting();
+
+
+
+// function printNumber(num) {
+//     console.log("Printing Number is:", num);
+// }
+// printNumber(5);
+
+
+function getAverage(num1, num2) {
+    let avg = (num1 + num2) / 2;
+    console.log("The avg value is:", avg);
+    
 }
-
-sayMyName();
+getAverage(5, 6);
