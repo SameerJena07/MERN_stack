ffb71960673173f510732cadbde1b8b83c5c676b
Sameer2005-dot <jena80225@gmail.com>
2026-02-12 01:35:15 +0530

    Added globalandFunction scope

---
+++ JS/lect46 DeadZone/index.js
@@ -0,0 +1,42 @@
+
+// Global Scope //
+
+// var age = 20;
+
+// let age = 22;
+
+// const age = 24;
+
+// console.log(age);
+
+// {
+//     console.log(age);
+// }
+
+// if(true){
+//     console.log(age);
+// }
+
+// for (let i = 1; i <= 2; i++){
+//     console.log(age);
+    
+// }
+
+// function sayMyAge() {
+//     console.log('My age is:',age);
+    
+// }
+// sayMyAge();
+
+
+
+// * Function Scope * //
+
+// function sayHello() {
+//     var fullName = "Sameer Jena";
+//     console.log("Hello Duniya!", fullName);
+    
+// }
+// console.log(fullName);
+
+// sayHello();
