b4234e4d7d27f8ed2658929685870baa129cb250
Sameer2005-dot <jena80225@gmail.com>
2026-02-15 03:08:16 +0530

    Added date object

--- JS/lect49 In-Built Object/index.js
+++ JS/lect49 In-Built Object/index.js
@@ -22,3 +22,33 @@
 // console.log(Math.sqrt(25));
 
 // console.log(Math.pow(2, 4));
+
+
+
+
+//* Date Object *//
+
+
+// let curr = new Date();
+// console.log(curr);
+
+// let date = new Date('July 07 2005 07:15');
+// console.log(date);
+
+let newDate = new Date(2005, 4, 7, 8);
+// console.log(newDate);
+
+
+// Get and Set //
+console.log(newDate.getDay());
+
+console.log(newDate.getFullYear());
+
+
+console.log(newDate.setFullYear(2007));
+console.log(newDate);
+
+
+
+
+
