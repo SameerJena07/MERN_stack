9c69365860bd5ccb7a52925374724164899b3daa
Sameer2005-dot <jena80225@gmail.com>
2026-02-14 23:44:21 +0530

    Added Default Parameters

--- JS/lect47 Classes/index.js
+++ JS/lect47 Classes/index.js
@@ -1,36 +1,69 @@
 
-class Human {
+//Default Parameter Value Passing
 
-    //properties
-    age = 13;  //public
-    #weight = 80;  //private
-    ht = 15;
+// function sayName(fName = "Shadow", lName = "YT"){
+//     console.log("My name is: ", fName, " ", lName);
+    
+// }
+// sayName("Sameer Jena");
 
-    constructor(newAge, newHeight, newWeigth){
-        this.age = newAge;
-        this.ht = newHeight;
-        this.#weight = newWeigth;
-    }
 
-    //behaviour
-    walking(){
-        console.log("I am Walking", this.#weight);
-    }
-    running(){
-        console.log("I am Running");
-        
-    }
+// function sayName(fName = "Uday", lName = fName.toUpperCase()){
+//     console.log("My name is: ", fName, " ", lName);
+    
+// }
+// sayName("sameer");
 
-    get fetchWeigth(){
-        return this.#weight;
-    }
 
-    set modifyWeight(val){
-        this.#weight = val;
-    }
+// Default Parameter of function have passing Object and Array easily
+
+// function solve(value = {age: 20, ht: 5.5, wt: 80 }){
+//     console.log("HELLO JII: ", value);
+    
+// }
+// solve();
+
+
+function solve(value = "Sameer"){
+    console.log("HELLO JII -> ", value);
+    
 }
-let obj = new Human(50, 190, 90);
+solve(undefined);
 
-console.log(obj.fetchWeigth);
-obj.walking();
+
+
+// class Human {
+
+//     //properties
+//     age = 13;  //public
+//     #weight = 80;  //private
+//     ht = 15;
+
+//     constructor(newAge, newHeight, newWeigth){
+//         this.age = newAge;
+//         this.ht = newHeight;
+//         this.#weight = newWeigth;
+//     }
+
+//     //behaviour
+//     walking(){
+//         console.log("I am Walking", this.#weight);
+//     }
+//     running(){
+//         console.log("I am Running");
+        
+//     }
+
+//     get fetchWeigth(){
+//         return this.#weight;
+//     }
+
+//     set modifyWeight(val){
+//         this.#weight = val;
+//     }
+// }
+// let obj = new Human(50, 190, 90);
+
+// console.log(obj.fetchWeigth);
+// obj.walking();
 
