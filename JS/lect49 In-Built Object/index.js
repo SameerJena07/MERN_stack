167966f1ebd8051592d0d2ff05982221b98f9425
Sameer2005-dot <jena80225@gmail.com>
2026-02-15 02:11:03 +0530

    Added math object

---
+++ JS/lect49 In-Built Object/index.js
@@ -0,0 +1,24 @@
+
+//* Math Object *//
+
+// console.log("Sameer");
+
+// console.log(Math.PI);
+
+// console.log(Math.max(60, 50, 122, 80));
+
+// console.log(Math.min(60, 50, 122, 80));
+
+// console.log(Math.round(1.4));
+
+// console.log(Math.floor(1.6));
+
+// console.log(Math.ceil(1.4));
+
+// console.log(Math.abs(-6));
+
+// console.log(Math.random());
+
+// console.log(Math.sqrt(25));
+
+// console.log(Math.pow(2, 4));
