4542228ac54f6683014a6d513507da82da2fb67e
Sameer2005-dot <jena80225@gmail.com>
2026-02-16 01:45:07 +0530

    update DOM manipulation

--- JS/lect51 DOMmanipulationHtml/index.js
+++ JS/lect51 DOMmanipulationHtml/index.js
@@ -1,9 +1,14 @@
 
 let mydiv = document.querySelector('#mydiv');
 
-let newElement = document.createElement('span');
-newElement.textContent = "This is me sameer";
+// let newElement = document.createElement('span');
+// newElement.textContent = "This is me sameer";
 
-mydiv.insertAdjacentElement('afterend', newElement);
+// mydiv.insertAdjacentElement('afterend', newElement);
 
+
+
+let parent = document.querySelector('#mydiv');
+let child = document.querySelector('#fpara');
+parent.removeChild(child);
 
