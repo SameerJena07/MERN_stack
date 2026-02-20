4594056625d84443a0c6e9c5ca6f4f364f59bf2e
Sameer2005-dot <jena80225@gmail.com>
2026-02-21 00:40:10 +0530

    Added documentFragment

--- JS/lect54 performance/index.js
+++ JS/lect54 performance/index.js
@@ -38,3 +38,17 @@
 /***/
 
 
+//best Code
+
+let fragment = document.createDocumentFragment();
+
+for (let i=1; i<=100; i++){
+    let para = document.createElement('p');
+    para.textContent = "This is para: " + i+1;
+
+    //No reflow and No Repaint for the below line
+    fragment.appendChild(para);
+}
+
+//The below line takes 1 reflow and 1 repaint
+document.body.appendChild(fragment);
