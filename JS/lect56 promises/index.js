9828a62d23125a6cd935aac2af9c559f60202d23
Sameer2005-dot <jena80225@gmail.com>
2026-02-22 03:21:29 +0530

    Added multiple promises

--- JS/lect56 promises/index.js
+++ JS/lect56 promises/index.js
@@ -27,32 +27,32 @@
 
 
 
-let promise1 = new Promise( (resolve, reject) => {
+// let promise1 = new Promise( (resolve, reject) => {
 
-    let success = true;
-    if(success){
-        resolve(10);
-    }
-    else {
-        reject(-1);
-    }
-});
+//     let success = true;
+//     if(success){
+//         resolve(10);
+//     }
+//     else {
+//         reject(-1);
+//     }
+// });
 
 
-promise1.then((message) => {
-    console.log("First msg: " + message);
-    return 20;
-}).then((message) => {
-    console.log("Second msg: " + message);
-    return 30;
-}).then((message) => {
-    console.log("Third msg: " + message);
-}).catch((error) => {
-    console.error(error);
-}).finally((message) => {
-    console.log("I am final, I can run");
+// promise1.then((message) => {
+//     console.log("First msg: " + message);
+//     return 20;
+// }).then((message) => {
+//     console.log("Second msg: " + message);
+//     return 30;
+// }).then((message) => {
+//     console.log("Third msg: " + message);
+// }).catch((error) => {
+//     console.error(error);
+// }).finally((message) => {
+//     console.log("I am final, I can run");
     
-})
+// })
 
 
 
@@ -63,3 +63,29 @@ promise1.then((message) => {
 // }).catch((error) => {
 //     console.log("Error: " + error);
 // })
+
+
+
+/***/
+
+
+//multiple promise//
+
+
+let promise1 = new Promise((resolve, reject) => {
+    setTimeout(resolve, 1000, "First");
+})
+let promise2 = new Promise((resolve, reject) => {
+    setTimeout(resolve, 2000, "Second");
+})
+let promise3 = new Promise((resolve, reject) => {
+    setTimeout(reject, 4000, "Third");
+})
+
+Promise.all([promise3, promise2, promise1])
+.then((value) => {
+    console.log(value);
+})
+.catch((error) => {
+    console.error("Error: " + error);
+})
