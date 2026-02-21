ce08485aeba475026dacbb58c1efc13b6fd7d0d3
Sameer2005-dot <jena80225@gmail.com>
2026-02-22 03:12:30 +0530

    Added then message

--- JS/lect56 promises/index.js
+++ JS/lect56 promises/index.js
@@ -25,3 +25,41 @@
 //     resolve(1);
 // });
 
+
+
+let promise1 = new Promise( (resolve, reject) => {
+
+    let success = true;
+    if(success){
+        resolve(10);
+    }
+    else {
+        reject(-1);
+    }
+});
+
+
+promise1.then((message) => {
+    console.log("First msg: " + message);
+    return 20;
+}).then((message) => {
+    console.log("Second msg: " + message);
+    return 30;
+}).then((message) => {
+    console.log("Third msg: " + message);
+}).catch((error) => {
+    console.error(error);
+}).finally((message) => {
+    console.log("I am final, I can run");
+    
+})
+
+
+
+
+
+// promise1.then((message) => {
+//     console.log("Then ka msg is " + message);
+// }).catch((error) => {
+//     console.log("Error: " + error);
+// })
