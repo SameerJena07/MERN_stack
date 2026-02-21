dd03cc9ed8c0c88252844973073f2ffe252a3324
Sameer2005-dot <jena80225@gmail.com>
2026-02-22 02:49:44 +0530

    Added promises in js

---
+++ JS/lect56 promises/index.js
@@ -0,0 +1,15 @@
+
+// * PROMISES * //
+
+
+// let firstPromise = new Promise( (resolve, reject) => {
+//     console.log("Sameer");
+
+//     // resolve(1001);
+
+//     reject(new Error("Internal Server Error!"));
+// });
+
+
+
+
