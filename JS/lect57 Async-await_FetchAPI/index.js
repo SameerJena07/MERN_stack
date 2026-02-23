6d5c9cdcd1dc7b62e86a093dd9adf19a9dbc7274
Sameer2005-dot <jena80225@gmail.com>
2026-02-23 23:45:54 +0530

    Added fetchAPI

--- JS/lect57 Async-await_FetchAPI/index.js
+++ JS/lect57 Async-await_FetchAPI/index.js
@@ -1,8 +1,32 @@
 
-async function getData (){
-    setTimeout(function (){
-        console.log("I am Inside set timeout block");
-    }, 3000);
-}
+// async function getData (){
+//     setTimeout(function (){
+//         console.log("I am Inside set timeout block");
+//     }, 3000);
+// }
 
+// getData();
+
+
+/// await ///
+
+///Fetch API///
+
+async function getData(){
+
+    //get request - async
+    let response = await fetch('https://jsonplaceholder.typicode.com/posts/1/comments');
+
+    //parse JSON - async
+    let data = await response.json();
+
+    console.log(data);
+    
+}
 getData();
+
+//Scenario:
+// prepare URL / API endpoint -> sync
+// fetch data -> network call -> async
+// process data -> sync
+
