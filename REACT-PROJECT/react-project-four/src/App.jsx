d14b78158e966f666e53db6de9eff4a987dd653c
Sameer2005-dot <jena80225@gmail.com>
2026-04-01 23:40:48 +0530

    Added props children

---
+++ REACT-PROJECT/react-project-four/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import Card from './components/card'
+
+function App() {
+
+  return (
+    <div>
+      <Card name="Sameer Jena">
+        <h1>I am Full Stack Developer</h1>
+        <p>I am from cse background</p>
+        <p>Now I am doing B.Tech</p>
+      </Card>
+    </div>
+  )
+}
+
+export default App
