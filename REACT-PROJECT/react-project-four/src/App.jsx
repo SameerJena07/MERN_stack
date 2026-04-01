4793e22b02a209e718a4f793800c56e49f81bf2b
Sameer2005-dot <jena80225@gmail.com>
2026-04-01 23:57:26 +0530

    Pass function inside children props

--- REACT-PROJECT/react-project-four/src/App.jsx
+++ REACT-PROJECT/react-project-four/src/App.jsx
@@ -1,16 +1,26 @@
 import { useState } from 'react'
 import './App.css'
 import Card from './components/card'
+import Button from './components/Button'
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  function handleClick (){
+    setCount(count+1);
+  }
 
   return (
     <div>
-      <Card name="Sameer Jena">
+      <Button handleClick={handleClick} text="click me">
+        <h1>{count}</h1>
+      </Button>
+
+      {/* <Card name="Sameer Jena">
         <h1>I am Full Stack Developer</h1>
         <p>I am from cse background</p>
         <p>Now I am doing B.Tech</p>
-      </Card>
+      </Card> */}
     </div>
   )
 }
