3f709ad17fd9c9e09d83229fe3216fef7bfe7c16
Sameer2005-dot <jena80225@gmail.com>
2026-04-04 14:18:22 +0530

    update event handling

--- REACT-PROJECT/Event_Handling/src/App.jsx
+++ REACT-PROJECT/Event_Handling/src/App.jsx
@@ -11,12 +11,27 @@ function App() {
     alert("The para is selected..")
   }
 
-  
+  function handleInputChange(e){
+    // console.log("The input value is changed");
+    console.log("The value till now:", e.target.value);
+  }
+
+  function handleSubmit(e){
+    e.preventDefault();
+    alert ("Form submit kardu kya?")
+  }
 
   return (
     <div>
 
-      
+      {/* <button onClick={alert("imidiate Invoke")}> 
+        Click Me
+      </button> */}
+
+      {/* <form onSubmit={handleSubmit}>
+        <input type="text" onChange={handleInputChange}/>
+        <button type='submit'>Submit</button>
+      </form> */}
 
       <p onMouseOver={handleMouseOver}>
         I am Paragraph
