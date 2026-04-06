e6aeebf748799015187c22f57ed33291230875b3
Sameer2005-dot <jena80225@gmail.com>
2026-04-06 23:49:10 +0530

    Update useContext

--- REACT-PROJECT/useContext/src/App.jsx
+++ REACT-PROJECT/useContext/src/App.jsx
@@ -1,16 +1,25 @@
-import { useState } from 'react'
+import { createContext, useState} from 'react'
 import './App.css'
+import FirstChild from './components/FirstChild';
 
-//create context
-const userContext = userContext();
+//step 1: create context
+const UserContext = createContext();
+//step 2: wrap all the context in a provider
+//step 3: pass value
+//step 4: make consume under a consumer
 
 function App() {
 
+  const [user, setUser] = useState({name:"Sameer"})
+
   return (
-    <div>
-      Hello Jee
-    </div>
+    <>
+      <UserContext.Provider value={user}>
+        <FirstChild/>
+      </UserContext.Provider> 
+    </>
   )
 }
 
 export default App
+export {UserContext}
