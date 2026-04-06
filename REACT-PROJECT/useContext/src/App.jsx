7ffecc54ae7a7473b7182a98112ced877d933582
Sameer2005-dot <jena80225@gmail.com>
2026-04-06 23:24:11 +0530

    Added useContext

---
+++ REACT-PROJECT/useContext/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+//create context
+const userContext = userContext();
+
+function App() {
+
+  return (
+    <div>
+      Hello Jee
+    </div>
+  )
+}
+
+export default App
